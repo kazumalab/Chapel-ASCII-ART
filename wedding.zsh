1f3a4fbb3b4a8e95063e28239120037cbb5f52ec
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-12-01 13:46:02 +0900

    Fixed: HereDocument and call echo once

--- wedding.zsh
+++ wedding.zsh
@@ -1,19 +1,24 @@
-COL=$(ruby << EOM
+col=$(ruby << EOM
 # frozen_string_literal: true
 puts [31, 32, 33, 34, 35, 36, 37].sample
 EOM
 )
-echo "  (  \e[31mHappy Wedding!!\e[m )                                      "
-echo "   (_________    ___)                   \e[0;${COL}m \ / \e[m         "
-echo "             \  /             _|_       \e[0;${COL}m  *  \e[m         "
-echo "              \/               |        \e[0;${COL}m / \ \e[m         "
-echo "                              / \       \e[37m  |  \e[m               "
-echo "                             /~~~\                                    "
-echo "                            /~[+]~\                                   "
-echo "                           /~~~~~~~\                                  "
-echo "                  \e[32m  $%&  \e[m /_________\                                 "
-echo "                  \e[32m #%$%$ \e[m |         |                                 "
-echo "                  \e[32m#$%&%$&\e[m |   ###   |                                 "
-echo "                  \e[32m &%&$& \e[m |   ###   |                                 "
-echo "          __________|_|___|___###___|_!-!_______                      "
-echo "                          /          \  https://www.mwed.jp           "
+
+chapel=`cat << Chapel
+  (  \e[31mHappy Wedding!!\e[m )                                      
+   (_________    ___)                   \e[0;${col}m \ / \e[m         
+             \  /             _|_       \e[0;${col}m  *  \e[m         
+              \/               |        \e[0;${col}m / \ \e[m         
+                              / \       \e[37m  |  \e[m               
+                             /~~~\                                    
+                            /~[+]~\                                   
+                           /~~~~~~~\                                  
+                  \e[32m  $%&  \e[m /_________\                       
+                  \e[32m #%$%$ \e[m |         |                       
+                  \e[32m#$%&%$&\e[m |   ###   |                       
+                  \e[32m &%&$& \e[m |   ###   |                        
+          __________|_|___|___###___|_!-!_______                      
+                          /          Minnna no Wedding AdventCalender!
+Chapel`
+
+echo -e $chapel
